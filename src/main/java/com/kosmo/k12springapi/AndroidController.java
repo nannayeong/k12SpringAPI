2622045fd8ece7474675fe8bda57dabac558381a
nannayeong <nannayeong@gmail.com>
2021-01-25 18:11:51 +0900

    JSONObject/JSONArray

--- src/main/java/com/kosmo/k12springapi/AndroidController.java
+++ src/main/java/com/kosmo/k12springapi/AndroidController.java
@@ -21,16 +21,26 @@ public class AndroidController {
 	@Autowired
 	private SqlSession SqlSession;
 	
-	@RequestMapping("/android/memberList.do")
+	/*1. 매개변수 필요 없이 JSONObject로 반환*/
+	@RequestMapping("/android/memberObject.do")
 	@ResponseBody
-	public ArrayList<MemberVO> memberList(HttpServletRequest req){
+	public Map<String, Object> memberList(HttpServletRequest req){
 		
 		Map<String, Object> map = new HashMap<String, Object>();
 		
 		ArrayList<MemberVO> lists = SqlSession.getMapper(IAndroidDAO.class).memberList();
 		map.put("memberList", lists);
 		
-		return lists;
+		return map;
+	}
+	
+	/*JSONArray로 데이터 반환*/
+	@RequestMapping("/android/memberList.do")
+	@ResponseBody
+	public ArrayList<MemberVO> memberObject(HttpServletRequest req){
 		
+		ArrayList<MemberVO> lists = SqlSession.getMapper(IAndroidDAO.class).memberList();
+		
+		return lists;
 	}
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -4,15 +4,24 @@
 <%@ page session="false" %>
 <html>
 <head>
-
-<h2>회원리스트 JSON으로 출력하기</h2>
-<ul>
-	<li>
-		<a href="./android/memberList.do" target="_blank">
-			회원리스트불러오기
-		</a>
-	</li>
-</ul>
-
+	<title>안드로이드연동</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+	<div class="container">	
+		<h2>회원리스트 JSON으로 출력하기</h2>
+		<ul>
+			<li>
+				<a href="./android/memberObject.do" target="_blank">
+					회원리스트불러오기(JSONObject)
+				</a>
+			</li>
+			<li>
+				<a href="./android/memberList.do" target="_blank">
+					회원리스트불러오기(JSONArray)
+				</a>
+			</li>
+		</ul>
+	</div>
 </body>
 </html>
